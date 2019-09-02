57deb5155a49210739b174ec435ae6a8c5dd58a8
wanghui <793147654@qq.com>
2019-09-02 21:02:49 +0800

    done 1

---
+++ my-project/src/components/HelloWorld.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <h2>{{title}}</h2>
+      <p>
+        <em>{{msg}}</em>
+      </p>
+    </div>
+    <input type="text" v-model="input">
+    <button @click = "increment">Add</button>
+    <ul>
+      <li v-for="n in parseInt(count)" :key="n">
+        <div>
+            <label><input type="checkbox"/>{{data[n - 1]}}</label>
+        </div>
+      </li>
+    </ul>
+    <button>ALL</button>
+    <button>Active</button>
+    <button>Complete</button>
+  </div>
+</template>
+
+<script>
+import Stylesheet from './Stylesheet'
+export default {
+  name: 'HelloWorld',
+  components: {
+    Stylesheet
+  },
+  data () {
+    return {
+      title: 'Jquery To Do List',
+      msg: 'Simple Todo List with adding and filter by diff status.',
+      count: 0,
+      input: null,
+      ALL: true,
+      Active: false,
+      Complete: false,
+      data: []
+    }
+  },
+  methods: {
+    increment () {
+      this.data.push(this.input)
+      this.count++
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+/* li {
+  display: inline-block;
+  margin: 0 10px;
+} */
+a {
+  color: #42b983;
+}
+</style>
---
+++ my-project/src/components/Stylesheet.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+        <div>
+            <h2>Jquery To Do List</h2>
+            <p>
+                <em>Simple Todo List with adding and filter by diff status.</em>
+            </p>
+        </div>
+        <div>
+            <input class="input-text" type="text" name="ListItem" />
+            <div id="button">Add</div>
+        </div>
+        <br/>
+        <ol></ol>
+        <div>
+            <ul id="filters">
+                <li>
+                    <a href="#" data-filter="all">ALL</a>
+                </li>
+                <li>
+                    <a href="#" data-filter="active">Active</a>
+                </li>
+                <li>
+                    <a href="#" data-filter="complete">Complete</a>
+                </li>
+            </ul>
+
+        </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Stylesheet',
+  data () {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
